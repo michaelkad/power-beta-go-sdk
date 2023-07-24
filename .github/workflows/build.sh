ab73dfa83fd066926680e3474edc4866af178aed
michaelkad <45772690+michaelkad@users.noreply.github.com>
2023-07-24 18:05:32 -0500

    Update build.sh

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -19,10 +19,10 @@ GIT_COMMIT=$(git rev-parse HEAD)
 GIT_DIRTY=$(test -n "`git status --porcelain`" && echo "+CHANGES" || true)
 
 # Determine the arch/os combos we're building for
-# XC_ARCH=${XC_ARCH:-"amd64" "arm64" "arm"}
-# XC_OS=${XC_OS:-linux darwin windows}
-XC_ARCH=${XC_ARCH:-"amd64" "arm64" }
-XC_OS=${XC_OS:-linux }
+XC_ARCH=${XC_ARCH:-"amd64" "arm64" "arm"}
+XC_OS=${XC_OS:-linux darwin windows}
+# XC_ARCH=${XC_ARCH:-"amd64" "arm64" }
+# XC_OS=${XC_OS:-linux }
 XC_EXCLUDE_OSARCH="!darwin/386 !windows/arm64 !windows/arm !darwin/arm"
 
 # Delete the old dir
